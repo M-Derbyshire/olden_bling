2cf1b338983445bfd554f8c35349efbafc090014
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-12-16 18:33:44 +0000

    makeChoice now uses numeric inputs, not letters

--- choice.h
+++ choice.h
@@ -7,16 +7,28 @@ typedef struct {
 
 int makeChoice(Choice options[], int optionCount)
 {
+    printf("What do you do?\n");
+    
     //Display choices
     for(int i = 0; i < optionCount; i++)
-        printf("%c: %s\n", 'a' + i, options[i].optionStr);
+        printf("(%d) %s\n", i + 1, options[i].optionStr);
     
-    //Make selection
+    
+    //Make selection (1, 2, 3, etc)
+    //Print a prompt, then repeat till a valid selection is made
     int selection = 0;
-    printf("> ");
-    while(selection < 'a' || selection >= ('a' + optionCount))
-        selection = getchar();
+    do
+    {
+        printf("> ");
+        int scanResult = scanf("%d", &selection);
+        
+        // if player didn't enter a number, we need to flush out the buffer
+        int c;
+        while((c = getchar()) != '\n' && c != EOF);
+        
+    } while(selection < 1 || selection > optionCount);
+    
     
     //Return selected function's index
-    return options[selection - 'a'].chosenSectionIndex;
+    return options[selection - 1].chosenSectionIndex;
 }
